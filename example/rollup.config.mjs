b545475cf90c9593aad4818a26722516aa00d075
xiaodu <ddz_job@outlook.com>
2022-11-06 21:25:45 +0800

    修改测试例子代码

--- example/rollup.config.mjs
+++ example/rollup.config.mjs
@@ -1,5 +1,20 @@
-//  npm i puppeteer-core rollup rollup-plugin-copy selenium-webdriver -D
-//  npm link a2bei4-rollup-plugin
+//#region   测试 npm 本地包
+
+//  包项目目录下
+//      包项目依赖 puppeteer-core rollup selenium-webdriver
+//      1、建立链接：npm link
+//      2、删除链接：npm unlink
+//          说明：开始删除时没有添加包名，报错了……所以需要添加包名
+//  测试项目下
+//      1、安装：
+//          npm i puppeteer-core rollup rollup-plugin-copy selenium-webdriver -D
+//          npm link a2bei4-rollup-plugin
+//          说明：使用本地包时不会顺便安装其依赖的其他包，所以这里要自己安装
+//      2、卸载：npm unlink --no-save a2bei4-rollup-plugin
+
+//#endregion
+
+//  npm i a2bei4-rollup-plugin rollup-plugin-copy
 //  https://blog.51cto.com/u_15127697/3633383
 
 import copy from "rollup-plugin-copy";
@@ -10,6 +25,7 @@ let seleniumBrowserOption = {
     driverPath: "D:\\ProgramOther\\edgedriver_win64\\msedgedriver.exe"
 };
 
+//  http://127.0.0.1:8899/reload-page-by-sse
 let sseOption = {
     hostname: "127.0.0.1",
     port: "8899",
@@ -52,12 +68,16 @@ let page1Plugins = [],
 
 //#endregion
 
-//#region   第三次尝试
+//#region   第三次尝试 SSE | WS 方案
 
 page1Plugins.push(reloadBySSE(sseOption));
 page2Plugins.push(reloadBySSE(sseOption));
 
 //#endregion
+
+//  【说明】
+//      1、使用第一种或者第二种方式时目前仅可以一个页面
+//      2、使用第三种方式时可以多个页面
 
 let page1Entry = {
     input: "./src/index.js",
